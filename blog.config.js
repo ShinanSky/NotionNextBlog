892b0157cc04131149690f4b9b5fe7a21833d5b7
ShinanSky <100468303+ShinanSky@users.noreply.github.com>
2026-08-16 23:59:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,10 +19,10 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'ShinanSky', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '是谁来自山川湖海，却囿于昼夜、厨房与爱？', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://shinansky.me', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Blog, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || "Blog, 博客, 世男Sky的个人博客, ShinanSky's Blog, ShinanSky, 世男Sky", // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/cusicon.jpg', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
-  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
-  BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || '', // 备案查询链接，如果用了萌备等备案请在这里填写
+  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '萌ICP备20258891', // 备案号 闽ICP备XXXXXX
+  BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://icp.gov.moe/?keyword=20258891', // 备案查询链接，如果用了萌备等备案请在这里填写
   BEI_AN_GONGAN: process.env.NEXT_PUBLIC_BEI_AN_GONGAN || '', // 公安备案号，例如 '浙公网安备3xxxxxxxx8号'
 
   // RSS订阅
